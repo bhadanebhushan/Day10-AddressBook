3b9f37d9e5c02a91bb0bc765331926eea2fecc1f
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-03-24 20:37:28 +0530

    Create Contact in address book

--- src/com/Addressbook/AddressBook.java
+++ src/com/Addressbook/AddressBook.java
@@ -1,7 +1,102 @@
 package com.Addressbook;
+class Contact {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private int zipcode;
+	private String state;
+	private String phoneNo;
+	private String email;
 
+	public Contact(String firstName, String lastName, String address, String city, int zipcode, String state,
+			String phoneNo, String email) {
+		setFirstName(firstName);
+		setLastName(lastName);
+		setAddress(address);
+		setCity(city);
+		setState(state);
+		setZipcode(zipcode);
+		setPhoneNo(phoneNo);
+		setEmail(email);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", zipcode=" + zipcode + ", state=" + state + ", phoneNo=" + phoneNo + ", email=" + email + "]";
+	}
+
+}
 public class AddressBook {
 	public static void main(String[] args) {
-		System.out.println("Welcome to address book");
+		System.out.println("Welcome to Address Book Programs");
+		Contact contact = new Contact(" Bhushan", "Bhadane", "21,xyz behind abc City", "Pune", 412101, "Maharashtra", "1234567890",
+				"xyz123@gmail.com");
+		System.out.println(contact);
 	}
 }
